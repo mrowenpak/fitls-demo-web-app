7481f2f07519937353eb53f99e04f1037f27beb5
Owen Pak <mr.owen.pak@gmail.com>
2024-05-28 20:44:32 +0300

    change content-type

--- app/routes/workouts.$workoutId_.start.tsx
+++ app/routes/workouts.$workoutId_.start.tsx
@@ -40,15 +40,18 @@ export default function Workout() {
 
     const fetchTest = async () => {
         try {
-            const data = {
+            const data : Record<string, any> = {
                 _auth: initData
             }
+
+            const formBody = Object.keys(data).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
+
             const response = await fetch('https://3196-188-32-34-191.ngrok-free.app/workout-complete', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
-                body: JSON.stringify(data)
+                body: formBody
             })
 
             if (!response.ok) {
@@ -82,6 +85,7 @@ export default function Workout() {
                         <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clipRule="evenodd" />
                     </svg>
                     <h1 className="text-md font-medium leading-none text-gray-500">{workout.name}</h1>
+                    <p>initData: {initData}</p>
                 </div>
                 <div className="mt-3 mb-5 p-4 bg-slate-100 rounded-lg">
                     <h2 className="text-lg font-bold leading-none text-gray-700">Задание</h2>
@@ -137,6 +141,7 @@ export default function Workout() {
                             <DrawerTitle>Отлично, ваши результаты:</DrawerTitle>
                             <DrawerDescription>
                                 Какая-то статистика
+
                             </DrawerDescription>
                         </DrawerHeader>
                         <DrawerFooter>
