0adeefef0adda12046db9356dca6c9b5cacbeba9
Owen Pak <mr.owen.pak@gmail.com>
2024-05-29 15:20:25 +0300

    test without WebApp.close()

--- app/routes/workouts.$workoutId_.start.tsx
+++ app/routes/workouts.$workoutId_.start.tsx
@@ -62,7 +62,8 @@ export default function Workout() {
             console.log('Failed to fetch from host', error);
         }
 
-        WebApp.close()
+        // @todo проверить - нужно ли это тут или вебапп само закрывается после успешного фетча?
+        // WebApp.close()
     };
 
     const handleClick = () => {
@@ -85,7 +86,6 @@ export default function Workout() {
                         <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clipRule="evenodd" />
                     </svg>
                     <h1 className="text-md font-medium leading-none text-gray-500">{workout.name}</h1>
-                    <p>initData: {initData}</p>
                 </div>
                 <div className="mt-3 mb-5 p-4 bg-slate-100 rounded-lg">
                     <h2 className="text-lg font-bold leading-none text-gray-700">Задание</h2>
